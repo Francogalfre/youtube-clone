9024fb488ce8d581d53efb4ad50a45d5192005f6
Franco Galfre <francogalfre2007@gmail.com>
2023-04-20 23:42:42 -0300

    fix: fix thumnail error

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,20 @@ function App() {
 	return (
 		<div>
 			<h1>Hello World</h1>
-			{videos.map((video: Video) => console.log(video.video.thumbnails[1].url))}
+			{videos.map(({ video }: Video) => (
+				<div key={video.videoId}>
+					<img
+						className='w-30 h-30 rounded-xl'
+						src={
+							video?.thumbnails?.[1]?.url
+								? video?.thumbnails?.[1]?.url
+								: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSimzQZZ8F6G5ANQfTH3Lt6xhKwaibOCG60RA8jZa_zDkLsiGxo5sYx5CgBfhzWdNTw4g&usqp=CAU"
+						}
+						alt={`Thumbnail of ${video.title}`}
+					/>
+					<span>{video.title}</span>
+				</div>
+			))}
 		</div>
 	)
 }
